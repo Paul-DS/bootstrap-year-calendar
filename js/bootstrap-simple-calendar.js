6000b88da585c1a344c59a3517631bc0d81b1914
Paul-DS <paul.david.sivelle@gmail.com>
2015-11-01 21:02:38 +0100

    Add year change management

--- js/bootstrap-simple-calendar.js
+++ js/bootstrap-simple-calendar.js
@@ -23,18 +23,25 @@
 		this.options = options;
 			
 		this.element.addClass('calendar');
-			
-		this.element.empty();
-		this._renderHeader();
-		this._renderYear();
-		this._renderMonth();
+		this._render();
 	};
  
 	Calendar.prototype = {
 		constructor: Calendar,
+		_render: function() {
+			this.element.empty();
+			this._renderHeader();
+			this._renderYear();
+			this.element.find('.months-container').hide();
+			this._renderMonth();
+			this._applyEvents();
+			this.element.find('.months-container').fadeIn(500);
+		},
 		_renderHeader: function() {
-			var header = $(document.createElement('table'));
-			header.addClass('calendar-header');
+			var header = $(document.createElement('div'));
+			header.addClass('calendar-header panel panel-default');
+			
+			var headerTable = $(document.createElement('table'));
 			
 			var prevDiv = $(document.createElement('th'));
 			prevDiv.addClass('prev');
@@ -44,13 +51,37 @@
 			
 			prevDiv.append(prevIcon);
 			
-			header.append(prevDiv);
+			headerTable.append(prevDiv);
 			
-			var titleDiv = $(document.createElement('th'));
-			titleDiv.addClass('year-title');
-			titleDiv.text(this.options.startYear);
+			var prev2YearDiv = $(document.createElement('th'));
+			prev2YearDiv.addClass('year-title year-neighbor2 hidden-sm hidden-xs');
+			prev2YearDiv.text(this.options.startYear - 2);
 			
-			header.append(titleDiv);
+			headerTable.append(prev2YearDiv);
+			
+			var prevYearDiv = $(document.createElement('th'));
+			prevYearDiv.addClass('year-title year-neighbor hidden-xs');
+			prevYearDiv.text(this.options.startYear - 1);
+			
+			headerTable.append(prevYearDiv);
+			
+			var yearDiv = $(document.createElement('th'));
+			yearDiv.addClass('year-title');
+			yearDiv.text(this.options.startYear);
+			
+			headerTable.append(yearDiv);
+			
+			var nextYearDiv = $(document.createElement('th'));
+			nextYearDiv.addClass('year-title year-neighbor hidden-xs');
+			nextYearDiv.text(this.options.startYear + 1);
+			
+			headerTable.append(nextYearDiv);
+			
+			var next2YearDiv = $(document.createElement('th'));
+			next2YearDiv.addClass('year-title year-neighbor2 hidden-sm hidden-xs');
+			next2YearDiv.text(this.options.startYear + 2);
+			
+			headerTable.append(next2YearDiv);
 			
 			var nextDiv = $(document.createElement('th'));
 			nextDiv.addClass('next');
@@ -60,22 +91,29 @@
 			
 			nextDiv.append(nextIcon);
 			
-			header.append(nextDiv);
+			headerTable.append(nextDiv);
+			
+			header.append(headerTable);
 			
 			this.element.append(header);
 		},
 		_renderYear: function() {
+			var monthsDiv = $(document.createElement('div'));
+			monthsDiv.addClass('months-container');
+			
 			for(var i = 0; i < 12; i++) {
 				var monthDiv = $(document.createElement('div'));
 				monthDiv.addClass('month-container col-lg-2 col-md-3 col-sm-3 col-xs-4')
 				monthDiv.data('monthId', i);
-				this.element.append(monthDiv);
+				monthsDiv.append(monthDiv);
 			}
+			
+			this.element.append(monthsDiv);
 		},
 		_renderMonth: function() {
 			var _this = this;
 		
-			this.element.children('.month-container').each(function() {
+			this.element.children('.months-container').children('.month-container').each(function() {
 				var monthNumber = $(this).data('monthId');
 				
 				var firstDate = new Date(_this.options.startYear, monthNumber, 1);
@@ -117,7 +155,7 @@
 				var currentDate = new Date(firstDate.getTime());
 				var lastDate = new Date(_this.options.startYear, monthNumber + 1, 0);
 				
-				while(currentDate.getUTCDay() != dates[_this.options.language].weekStart)
+				while(currentDate.getDay() != dates[_this.options.language].weekStart)
 				{
 					currentDate.setDate(currentDate.getDate() - 1);
 				}
@@ -130,21 +168,22 @@
 					{
 						var cell = $(document.createElement('td'));
 						cell.addClass('day');
-						cell.text(currentDate.getDate());
 						
 						if(currentDate < firstDate) {
 							cell.addClass('old');
-							debugger
 						}
 						else if(currentDate > lastDate) {
 							cell.addClass('new');
+						}
+						else {
+							cell.text(currentDate.getDate());
 						}
 						
 						row.append(cell);
 						
 						currentDate.setDate(currentDate.getDate() + 1);
 					}
-					while(currentDate.getUTCDay() != dates[_this.options.language].weekStart)
+					while(currentDate.getDay() != dates[_this.options.language].weekStart)
 					
 					table.append(row);
 				}
@@ -152,28 +191,32 @@
 				$(this).append(table);
 			});
 		},
-			if (!date)
-				return '';
-			var val = {
-				d: date.getUTCDate(),
-				D: dates[this.options.language].daysShort[date.getUTCDay()],
-				DD: dates[this.options.language].days[date.getUTCDay()],
-				m: date.getUTCMonth() + 1,
-				M: dates[this.options.language].monthsShort[date.getUTCMonth()],
-				MM: dates[this.options.language].months[date.getUTCMonth()],
-				yy: date.getUTCFullYear().toString().substring(2),
-				yyyy: date.getUTCFullYear()
-			};
-			val.dd = (val.d < 10 ? '0' : '') + val.d;
-			val.mm = (val.m < 10 ? '0' : '') + val.m;
-			date = [];
-			var seps = $.extend([], format.separators);
-			for (var i=0, cnt = format.parts.length; i <= cnt; i++){
-				if (seps.length)
-					date.push(seps.shift());
-				date.push(val[format.parts[i]]);
-			}
-			return date.join('');
+		_applyEvents: function () {
+			var _this = this;
+			this.element.find('.year-neighbor, .year-neighbor2').click(function() {
+				_this.setYear(parseInt($(this).text()));
+			});
+			
+			this.element.find('.calendar-header .prev').click(function() {
+				_this.element.find('.months-container').animate({'margin-left':'100%'},100, function() {
+					_this.element.find('.months-container').hide();
+					_this.element.find('.months-container').css('margin-left', '0');
+					_this.setYear(_this.options.startYear - 1);
+				});
+				
+			});
+			
+			this.element.find('.calendar-header .next').click(function() {
+				_this.element.find('.months-container').animate({'margin-left':'-100%'},100, function() {
+					_this.element.find('.months-container').hide();
+					_this.element.find('.months-container').css('margin-left', '0');
+					_this.setYear(_this.options.startYear + 1);
+				});
+			});
+		},
+		setYear: function(year) {
+			this.options.startYear = year;
+			this._render();
 		}
 	}
  
