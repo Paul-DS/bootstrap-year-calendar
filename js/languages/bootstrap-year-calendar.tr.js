2652f6dbcab634f67c7cb41ef5874a773c402249
Paul-DS <paul.david.sivelle@gmail.com>
2015-12-23 22:05:23 +0100

    Implement new languages.
    Languages implemented :
    - Dutch
    - German
    - Italian
    - Japanese
    - Portuguese
    - Russian
    - Spanish
    - Turkish

---
+++ js/languages/bootstrap-year-calendar.tr.js
@@ -0,0 +1,19 @@
+/**
+ * Turkish translation for bootstrap-year-calendar
+ * Paul DAVID-SIVELLE
+ * Based on 
+ * Turkish translation for bootstrap-datepicker
+ * Serkan Algur <kaisercrazy_2@hotmail.com>
+ */
+ 
+;(function($){
+	$.fn.calendar.dates['tr'] = {
+		days: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+		daysShort: ["Pz", "Pzt", "Sal", "Çrş", "Prş", "Cu", "Cts"],
+		daysMin: ["Pz", "Pzt", "Sa", "Çr", "Pr", "Cu", "Ct"],
+		months: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+		monthsShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz", "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+		weekStart: 1
+	};
+}(jQuery));
+
