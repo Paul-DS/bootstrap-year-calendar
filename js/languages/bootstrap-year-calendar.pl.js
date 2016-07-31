f4981061811c6d7ff97c6b98708109a5b8d57acd
Paul-DS <Paul-DS@users.noreply.github.com>
2016-07-31 18:58:59 +0200

    #35: Add Polish language file

---
+++ js/languages/bootstrap-year-calendar.pl.js
@@ -0,0 +1,16 @@
+/**
+ * Polish translation for bootstrap-year-calendar
+ * Robert 'Wooya' Gaudyn
+ */
+
+;(function($){
+    $.fn.calendar.dates['pl'] = {
+        days: ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piatek", "Sobota"],
+        daysShort: ["Nie", "Pon", "Wto", "Śro", "Czw", "Pią", "Sob"],
+        daysMin: ["Ni", "Po", "Wt", "Śr", "Cz", "Pi", "So"],
+        months: ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"],
+        monthsShort: ["Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Paź", "Lis", "Gru"],
+        weekShort: 'W',
+        weekStart: 1
+    };
+}(jQuery));
