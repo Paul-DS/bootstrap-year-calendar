c6f80bd829f3f4c212324c9d86aa9994c4b3cbd4
rytisjukna <rytis.jukna@gmail.com>
2016-12-30 22:19:00 +0200

    added Lithuanian translation, with proper permissive license

--- js/languages/bootstrap-year-calendar.lt.js
+++ js/languages/bootstrap-year-calendar.lt.js
@@ -1,16 +1,15 @@
 ﻿/**
- * German translation for bootstrap-year-calendar
- * Paul DAVID-SIVELLE
- * Based on 
- * German translation for bootstrap-datepicker
- * Sam Zurcher <sam@orelias.ch>
+ * Lithuanian translation for bootstrap-year-calendar
+ * Mantas Urbonas mantas.urbonas@gmail.com
+ *
+ * either MIT or BSD or Apache 2 licensed - choose whatever license suits you most.
  */
  
 ;(function($){
 	$.fn.calendar.dates['lt'] = {
 		days: ["Sekmadienis", "Pirmadienis", "Antradienis", "Trečiadienis", "Ketvirtadienis", "Penktadienis", "Šeštadienis"],
 		daysShort: ["Sek", "Pir", "Ant", "Tre", "Ket", "Pen", "Šeš"],
-		daysMin: ["Se", "P", "An", "Tr", "Kt", "Pn", "Še"],
+		daysMin: ["Se", "Pr", "An", "Tr", "Kt", "Pn", "Še"],
 		months: ["Sausis", "Vasaris", "Kovas", "Balandis", "Gegužė", "Birželis", "Liepa", "Rugpjūtis", "Rugsėjis", "Spalis", "Lapkritis", "Gruodis"],
 		monthsShort: ["Sau", "Vas", "Kov", "Bal", "Geg", "Bir", "Lie", "Rgp", "Rug", "Spa", "Lap", "Gru"],
 		weekShort: 'Sav',
