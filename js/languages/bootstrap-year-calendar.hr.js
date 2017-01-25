eeb2e0b58a40183cd5e9e7f634403fec2773bf4c
dd <davor.doslinec@gmail.com>
2017-01-25 15:22:04 +0100

    Add files via upload

---
+++ js/languages/bootstrap-year-calendar.hr.js
@@ -0,0 +1,16 @@
+/**
+ * Croatian translation for bootstrap-year-calendar
+ * Davor Došlnec <davor.doslinec@gmail.com>
+ */
+
+;(function($){
+    $.fn.calendar.dates['hr'] = {
+        days: ["Nedjelja", "Ponedjeljak", "Utorak", "Srijeda", "Četvrtak", "Petak", "Subota"],
+        daysShort: ["Ned", "Pon", "Uto", "Sri", "Čet", "Pet", "Sub"],
+        daysMin: ["Ne", "Po", "Ut", "Sr", "Če", "Pe", "Su"],
+        months: ["Siječanj", "Veljača", "Ožujak", "Travanj", "Svibanj", "Lipanj", "Srpanj", "Kolovoz", "Rujan", "Listopad", "Studeni", "Prosinac"],
+        monthsShort: ["Sij", "Vel", "Ožu", "Tra", "Svi", "Lip", "Srp", "Kol", "Ruj", "Lis", "Stu", "Pro"],
+        weekShort: 'T',
+        weekStart: 1
+    };
+}(jQuery));
