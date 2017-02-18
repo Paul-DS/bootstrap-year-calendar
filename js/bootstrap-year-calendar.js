Paul-DS/bootstrap-year-calendar

commit 709c3bea2d79c8eeb7a178f744d112f059eaeb7f

Replace all the "for" loop (use the original syntax), in order to avoid errors when looping on extended arrays

js/bootstrap-year-calendar.js
@@ -74,7 +74,7 @@
 			if(opt.mouseOutDay) { this.element.bind('mouseOutDay', opt.mouseOutDay); }
 		},
 		_initializeDatasourceColors: function() {
-			for(var i in this.options.dataSource) {
+			for(var i = 0; i < this.options.dataSource.length; i++) {
 				if(this.options.dataSource[i].color == null) {
 					this.options.dataSource[i].color = colors[i % colors.length];
 				}
@@ -318,7 +318,7 @@
 					{
 						var monthData = [];
 					
-						for(var i in _this.options.dataSource) {
+						for(var i = 0; i < _this.options.dataSource.length; i++) {
 							if(!(_this.options.dataSource[i].startDate >= lastDate) || (_this.options.dataSource[i].endDate < firstDate)) {
 								monthData.push(_this.options.dataSource[i]);
 							}
@@ -333,7 +333,7 @@
 								
 								if((_this.options.minDate == null || currentDate >= _this.options.minDate) && (_this.options.maxDate == null || currentDate <= _this.options.maxDate))
 								{
-									for(var i in monthData) {
+									for(var i = 0; i < monthData.length; i++) {
 										if(monthData[i].startDate < nextDate && monthData[i].endDate >= currentDate) {
 											dayData.push(monthData[i]);
 										}
@@ -686,7 +686,7 @@
 			var date = this._getDate(elt);
 			var events = this.getEvents(date);
 			
-			for(var i in events) {
+			for(var i = 0; i < events.length; i++) {
 				var eventItem = $(document.createElement('div'));
 				eventItem.addClass('item');
 				eventItem.css('border-left', '4px solid ' + events[i].color);
@@ -722,7 +722,7 @@
 			var subMenu = $(document.createElement('div'));
 			subMenu.addClass('submenu');
 			
-			for(var i in items) {
+			for(var i = 0; i < items.length; i++) {
 				if(!items[i].visible || items[i].visible(evt)) {
 					var menuItem = $(document.createElement('div'));
 					menuItem.addClass('item');
@@ -774,7 +774,7 @@
 		_triggerEvent: function(eventName, parameters) {
 			var event = $.Event(eventName);
 			
-			for(var i in parameters) {
+			for(var i = 0; i < parameters.length; i++) {
 				event[i] = parameters[i];
 			}
 			
@@ -789,7 +789,7 @@
 			}
 			
 			if(this.options.disabledWeekDays.length > 0) {
-				for(var d in this.options.disabledWeekDays){
+				for(var d = 0; d < this.options.disabledWeekDays.length; d++){
 					if(date.getDay() == this.options.disabledWeekDays[d]) {
 						return true;
 					}
@@ -797,7 +797,7 @@
 			}
 			
 			if(this.options.disabledDays.length > 0) {
-				for(var d in this.options.disabledDays){
+				for(var d = 0; d < this.options.disabledDays.length; d++){
 					if(date.getTime() == this.options.disabledDays[d].getTime()) {
 						return true;
 					}
@@ -808,7 +808,7 @@
 		},
 		_isHidden: function(day) {
 			if(this.options.hiddenWeekDays.length > 0) {
-				for(var d in this.options.hiddenWeekDays) {
+				for(var d = 0; d < this.options.hiddenWeekDays.length; d++) {
 					if(day == this.options.hiddenWeekDays[d]) {
 						return true;
 					}
@@ -831,7 +831,7 @@
 			var events = [];
 			
 			if(this.options.dataSource && startDate && endDate) {
-				for(var i in this.options.dataSource) {
+				for(var i = 0; i < this.options.dataSource.length; i++) {
 					if(this.options.dataSource[i].startDate < endDate && this.options.dataSource[i].endDate >= startDate) {
 						events.push(this.options.dataSource[i]);
 					}

commit 8ab60d22bab8048ca62f5fbaad14ec2c3fdf640b

Fix a bug when computing parameters for calendar events (introduced in commit 709c3bea2d79c8eeb7a178f744d112f059eaeb7f)

js/bootstrap-year-calendar.js
@@ -774,7 +774,7 @@
 		_triggerEvent: function(eventName, parameters) {
 			var event = $.Event(eventName);
 			
-			for(var i = 0; i < parameters.length; i++) {
+			for(var i in parameters) {
 				event[i] = parameters[i];
 			}
 			