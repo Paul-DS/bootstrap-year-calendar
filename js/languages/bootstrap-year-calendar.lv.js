205134b1438695ac6803780a2a94966a06e23e15
Imants Horsts <DarkSide666@users.noreply.github.com>
2017-02-19 22:58:34 +0200

    Adds Latvian translation

---
+++ js/languages/bootstrap-year-calendar.lv.js
@@ -0,0 +1,16 @@
+/**
+ * Latvian translation for bootstrap-year-calendar
+ * Imants Horsts
+ */
+
+;(function($){
+	$.fn.calendar.dates['lv'] = {
+		days: ["Svētdiena", "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena"],
+		daysShort: ["Svt", "Prm", "Otr", "Tre", "Ctr", "Pkt", "Sst"],
+		daysMin: ["Sv", "P", "O", "T", "C", "Pk", "S"],
+		months: ["Janvāris", "Februāris", "Marts", "Aprīlis", "Maijs", "Jūnijs", "Jūlijs", "Augusts", "Septembris", "Oktobris", "Novembris", "Decembris"],
+		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mai", "Jūn", "Jūl", "Aug", "Sep", "Okt", "Nov", "Dec"],
+		weekShort: 'Ned.',
+		weekStart: 1
+	};
+}(jQuery));
