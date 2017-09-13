ffb2e4c2ff94b2f67e30c93263b2c66f4e1e6ebd
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2017-09-13 17:09:18 +0300

    Finnish translation

---
+++ js/languages/bootstrap-year-calendar.fi.js
@@ -0,0 +1,16 @@
+/**
+ * Finnish translation for bootstrap-year-calendar
+ * Tuomas Jaakola (iqqmuT) <tuomas.jaakola@iki.fi>
+ */
+
+;(function($){
+	$.fn.calendar.dates['fi'] = {
+		days: ["sunnuntai", "maanantai", "tiistai", "keskiviikko", "torstai", "perjantai", "lauantai"],
+		daysShort: ["su", "ma", "ti", "ke", "to", "pe", "la"],
+		daysMin: ["su", "ma", "ti", "ke", "to", "pe", "la"],
+		months: ["tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu"],
+		monthsShort: ["tammi", "helmi", "maalis", "huhti", "touko", "kesä", "heinä", "elo", "syys", "loka", "marras", "joulu"],
+		weekShort: 'vko',
+		weekStart: 1
+	};
+}(jQuery));
