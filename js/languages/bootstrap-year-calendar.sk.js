a8dd548f6a1203b694a043b104cc84ea35ae2634
Šulgan Marián <marian.sulgan@gmail.com>
2017-11-10 13:30:49 +0100

    Slovak language support added

---
+++ js/languages/bootstrap-year-calendar.sk.js
@@ -0,0 +1,19 @@
+/**
+ * German translation for bootstrap-year-calendar
+ * Paul DAVID-SIVELLE
+ * Based on 
+ * German translation for bootstrap-datepicker
+ * Sam Zurcher <sam@orelias.ch>
+ */
+ 
+;(function($){
+	$.fn.calendar.dates['sk'] = {
+		days: ["Nedeľa", "Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok", "Sobota"],
+		daysShort: ["Ned", "Pon", "Uto", "Str", "Štv", "Pia", "Sob"],
+		daysMin: ["Ne", "Po", "Ut", "St", "Št", "Pia", "So"],
+		months: ["Január", "Február", "Marec", "Apríl", "Máj", "Jún", "Júl", "August", "September", "Október", "November", "December"],
+		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Máj", "Jún", "Júl", "Aug", "Sep", "Okt", "Nov", "Dec"],
+		weekShort: 'W',
+		weekStart: 1
+	};
+}(jQuery));
