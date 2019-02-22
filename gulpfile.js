951c5fb80f603fd50c6e7bcfd5f85a208398bc16
Paul-DS <paul.david.sivelle@gmail.com>
2019-02-22 23:31:31 +0100

    Add minification during build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const less = require('gulp-less');
+const uglify = require('gulp-uglify');
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 function scripts() {
 	return gulp.src('src/js/js-year-calendar.js')
         .pipe(babel())
+        .pipe(gulp.dest('dist'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
         .pipe(gulp.dest('dist'));
 }
 
 function styles() {
 	return gulp.src('src/less/js-year-calendar.less')
         .pipe(less())
+        .pipe(gulp.dest('dist'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cleanCss())
         .pipe(gulp.dest('dist'));
 }
 
