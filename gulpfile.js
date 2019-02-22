3553dbf7f9dee420121a69f26be6c42722bb7764
Paul-DS <paul.david.sivelle@gmail.com>
2019-02-22 23:21:21 +0100

    Moved source code in a "src" folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const babel = require('gulp-babel');
 const less = require('gulp-less');
 
 function scripts() {
-	return gulp.src('js/js-year-calendar.js')
+	return gulp.src('src/js/js-year-calendar.js')
         .pipe(babel())
         .pipe(gulp.dest('dist'));
 }
 
 function styles() {
-	return gulp.src('css/js-year-calendar.less')
+	return gulp.src('src/less/js-year-calendar.less')
         .pipe(less())
         .pipe(gulp.dest('dist'));
 }
@@ -17,6 +17,6 @@ function styles() {
 gulp.task('build', gulp.series(scripts, styles));
 
 gulp.task('watch', function () {
-    gulp.watch('js/**/*.js', scripts);
-    gulp.watch('less/**/*.less', styles);
+    gulp.watch('src/js/**/*.js', scripts);
+    gulp.watch('src/less/**/*.less', styles);
 });
