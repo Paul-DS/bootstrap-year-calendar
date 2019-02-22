b200b7a41905419ca67994dfb6d2a78ab941149c
Paul-DS <paul.david.sivelle@gmail.com>
2019-02-22 23:18:13 +0100

    Use less for better stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const watch = require('gulp-watch');
+const less = require('gulp-less');
 
-// task
-gulp.task('build', function () {
+function scripts() {
 	return gulp.src('js/js-year-calendar.js')
         .pipe(babel())
         .pipe(gulp.dest('dist'));
-});
+}
+
+function styles() {
+	return gulp.src('css/js-year-calendar.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist'));
+}
+
+gulp.task('build', gulp.series(scripts, styles));
 
 gulp.task('watch', function () {
-    return watch('js/**/*.js', function () {
-        return gulp.src('js/js-year-calendar.js')
-            .pipe(babel())
-            .pipe(gulp.dest('dist'));
-    });
+    gulp.watch('js/**/*.js', scripts);
+    gulp.watch('less/**/*.less', styles);
 });
