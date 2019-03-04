6b55763ab8a73f5891ab46ef1f373739697fa4d4
Paul-DS <paul.david.sivelle@gmail.com>
2019-03-04 16:43:21 +0100

    Add unit test for the automatic calendar initialization

---
+++ tests/auto-init.js
@@ -0,0 +1,12 @@
+require('../dist/js-year-calendar');
+
+test('instantiate calendar with selector', () => {
+    document.body.innerHTML = '<div id="calendar" data-provide="calendar"></div>';
+
+    // Manually trigger the DOMContentLoaded event
+    var event = document.createEvent("Event");
+    event.initEvent("DOMContentLoaded", true, true);
+    document.dispatchEvent(event);
+
+    expect(document.querySelector('#calendar').children.length).toEqual(2);
+});
