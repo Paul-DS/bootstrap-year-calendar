2aa7696d6b8c438b3b78430ad215dd415b10a44a
mhk1014 <58805927+mhk1014@users.noreply.github.com>
2020-05-22 16:25:00 +0900

    Create bootstrap-year-calendar.ko.js

---
+++ locales/bootstrap-year-calendar.ko.js
@@ -0,0 +1,15 @@
+/**
+korean translation for bootstrap-year-calendar
+minho kim <alsghek112@gmail.com>
+*/
+;(function($){
+$.fn.calendar.dates['ko'] = {
+days: ["일", "월", "화", "수", "목", "금", "토"],
+daysShort: ["일", "월", "화", "수", "목", "금", "토"],
+daysMin: ["일", "월", "화", "수", "목", "금", "토"],
+months: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+monthsShort: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+weekShort: 'T',
+weekStart: 1
+};
+}(jQuery));
