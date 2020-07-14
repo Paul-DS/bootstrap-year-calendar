2c6e0e0e6c6ea27591a8ab505b6388ebd8c9a7ab
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2020-07-14 17:08:30 +0800

    Update js-year-calendar.ms_MY.js

--- locales/js-year-calendar.ms_MY.js
+++ locales/js-year-calendar.ms_MY.js
@@ -4,8 +4,8 @@ Chia Wei Lim <acelimcw@hotmail.com>
 */
 Calendar.locales['ms_MY'] = {
 	days: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
-	daysShort: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
-	daysMin: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
+	daysShort: ["Ahd", "Isn", "Sel", "Rab", "Kha", "Jum", "Sab"],
+	daysMin: ["Ahd", "Isn", "Sel", "Rab", "Kha", "Jum", "Sab"],
 	months: ["Januari", "Februari", "Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "Oktober", "November", "Disember"],
 	monthsShort: ["Jan", "Feb", "Mac", "Apr", "Mei", "Jun", "Jul", "Ogos", "Sep", "Okt", "Nov", "Dis"],
 	weekShort: 'Minggu',
