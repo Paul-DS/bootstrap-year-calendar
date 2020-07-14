edb7d2f10da34c9e38312102af18e8beca8b2ca3
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2020-07-14 16:42:54 +0800

    Create js-year-calendar.my.js

---
+++ locales/js-year-calendar.my.js
@@ -0,0 +1,13 @@
+/**
+Malay translation for bootstrap-year-calendar
+Chia Wei Lim <acelimcw@hotmail.com>
+*/
+Calendar.locales['my'] = {
+	days: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
+	daysShort: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
+	daysMin: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
+	months: ["Januari", "Februari", "Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "Oktober", "November", "Disember"],
+	monthsShort: ["Jan", "Feb", "Mac", "April", "Mei", "Jun", "Julai", "Ogos", "Sept", "Okt", "Nov", "Dis"],
+	weekShort: 'Minggu',
+	weekStart: 1
+};
