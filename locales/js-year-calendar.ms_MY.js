0db02bbab052ae70729b9ef124633acd9bda6a24
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2020-07-14 16:56:17 +0800

    Update and rename js-year-calendar.my.js to js-year-calendar.ms_MY.js

--- locales/js-year-calendar.ms_MY.js
+++ locales/js-year-calendar.ms_MY.js
@@ -2,12 +2,12 @@
 Malay translation for bootstrap-year-calendar
 Chia Wei Lim <acelimcw@hotmail.com>
 */
-Calendar.locales['my'] = {
+Calendar.locales['ms_MY'] = {
 	days: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
 	daysShort: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
 	daysMin: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
 	months: ["Januari", "Februari", "Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "Oktober", "November", "Disember"],
-	monthsShort: ["Jan", "Feb", "Mac", "April", "Mei", "Jun", "Julai", "Ogos", "Sept", "Okt", "Nov", "Dis"],
+	monthsShort: ["Jan", "Feb", "Mac", "Apr", "Mei", "Jun", "Jul", "Ogos", "Sep", "Okt", "Nov", "Dis"],
 	weekShort: 'Minggu',
 	weekStart: 1
 };
