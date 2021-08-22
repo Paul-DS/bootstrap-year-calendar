f1a051e6611f5e25db3dfa605c386551b8706464
Paul-DS <Paul-DS@users.noreply.github.com>
2021-08-22 12:30:44 +0200

    Improve the way to get the weekstart

--- src/ts/js-year-calendar.ts
+++ src/ts/js-year-calendar.ts
@@ -446,7 +446,7 @@ export default class Calendar<T extends CalendarDataSourceElement> {
 				headerRow.appendChild(weekNumberCell);
 			}
 			
-			var weekStart = !isNaN(this.options.weekStart) ? this.options.weekStart : Calendar.locales[this.options.language].weekStart;
+			var weekStart = this.getWeekStart();
 			var d = weekStart;
 			do
 			{
@@ -1766,7 +1766,7 @@ export default class Calendar<T extends CalendarDataSourceElement> {
      * Gets the starting day of the week.
      */
 	public getWeekStart(): number {
-		return !isNaN(this.options.weekStart) ? this.options.weekStart : Calendar.locales[this.options.language].weekStart;
+		return this.options.weekStart !== null ? this.options.weekStart : Calendar.locales[this.options.language].weekStart;
 	}
 
 	/**
